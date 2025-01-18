1e8cabe46711a35f74dc9625bef33ed2055b9647
sanvirahman <sheikh15-3700@diu.edu.bd>
2025-01-18 09:18:50 +0600

    init now

--- forms/views.py
+++ forms/views.py
@@ -10,35 +10,45 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "Your registration in Successful")
+            messages.success(request, "User created successfully")
             return redirect("login")
     else:
         form = CustomUserCreationForm()
     return render(request, "register.html", {"form": form})
 
+
+
 def user_login(request):
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
-        user = authenticate(request, email=email, password=password)
+
+        user= authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "You are logged in Successfully")
             return redirect("dashboard")
-            
         else:
-            return render(request, "login.html", {"error": "Invalid email or password"})
+            messages.error(request, "Invalid email or password")
+            return render(request, "login.html")
+
     return render(request, "login.html")
+
+
 
 @login_required(login_url="login")
 def dashboard(request):
-    messages.success(request, "You are now logged out Successfully")
+    messages.success(request, "You are logged in Successfully")
     return render(request, "dashboard.html", {"user": request.user})
-        
+
+
 
 def user_logout(request):
     logout(request)
+    messages.success(request, "You are logged out Successfully")
     return redirect("login")
+
+
+
 
 
 
